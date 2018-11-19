3350c18a488bc0920130936f3e0f767dda200740
Nikolay Piskarev <nikolay_piskarev@epam.com>
2018-11-19 20:17:22 +0400

    Added MyCalc project.

---
+++ My Calc/My Calc/Models/Operation.cs
@@ -0,0 +1,12 @@
+﻿using My_Calc.Resources;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Calc.Models
+{
+    public enum Operation
+    {
+        [Display(Name = "Add", ResourceType = typeof(CalcResources))]
+        Add
+    }
+}
